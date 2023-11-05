8b347d180174c037b1d1f642b25927cb8dfeaaf2
abtyagi15 <abtyagi150702@gmail.com>
2023-11-06 00:31:21 +0530

    created routes for delete, updating profile and changing profile picture, getting user details

--- server/routes/Profile.js
+++ server/routes/Profile.js
@@ -3,4 +3,25 @@ const router = express.Router();
 
 
 //import controllers
-const {updatProfile,deleteProfile} = require("../controllers/Profile");
+const {updatProfile,deleteProfile,getAllUserDetails,getEnrolledCourses,updateDispalyPicture} = require("../controllers/Profile");
+
+//import middlewares
+const {auth} = require("../middlewares/auth");
+
+
+//........................................................................................................//
+//                                       profile routes                                                    //
+//........................................................................................................//
+
+//route for updating profile
+router.put('/update-profile',auth,updatProfile);
+//route for deleting the account
+router.delete('/delete-profile',auth,deleteProfile);
+//route for getting all the details of an user
+router.get('/get-user-details',auth,getAllUserDetails);
+//router for getting all the courses in which an user is enrolled
+router.get('/get-enrolled-courses',auth,getEnrolledCourses);
+//route for updating user profile picture
+router.put('/update-profile-picture',auth,updateDispalyPicture);
+
+module.exports = router;
